39217f2718dba962f819c5257462429e0ab70be7
Nivaldeir Silva <nivaldeir-silva@hotmail.com>
2024-01-11 22:08:01 -0300

    chore: remove loggers

--- src/infra/queue/rabbitmq/RabbitMqAdapter.ts
+++ src/infra/queue/rabbitmq/RabbitMqAdapter.ts
@@ -2,17 +2,12 @@ import logger from "../../logger";
 import { Queue } from "../Queue";
 import amqp from "amqplib"
 export class RabbitMQAdapter implements Queue {
-  connection: any
+  private connection: any
   consume(name: string, callback: Function): Promise<void> {
     throw new Error("Method not implemented.");
   }
   async connect(): Promise<void> {
-    try {
-      this.connection = await amqp.connect(`amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_USER}@${process.env.RABBITMQ_URL}`);
-      logger.info("[RABBITMQ]: Connected to RabbitMQ")
-    } catch (error) {
-      logger.error("[RABBITMQ]: Error to conneted RabbitMQ")
-    }
+    this.connection = await amqp.connect(`amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_USER}@${process.env.RABBITMQ_URL}`);
   }
   async publish(name: string, data: any): Promise<void> {
     const channel = await this.connection.createChannel();
