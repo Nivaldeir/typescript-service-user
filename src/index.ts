80ebc978c8c4207a915ed565823cd389c0e025fd
Nivaldeir Silva <nivaldeir-silva@hotmail.com>
2024-01-11 22:08:32 -0300

    chore: create new functionthis RabbitMQ

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,25 @@ import { MainController } from "./infra/http/MainController";
 import { EmailExternal } from "./infra/Email/EmailExternal";
 import { RedisAdapter } from "./infra/cache/RedisAdapter";
 import { RabbitMQAdapter } from "./infra/queue/rabbitmq/RabbitMqAdapter";
+import logger from "./infra/logger";
 
 async function main() {
+  const rabbitMq = new RabbitMQAdapter()
+  await connectingToRabbitMQ(rabbitMq)
   const httpServer = new ExpressAdapter()
   const client = new PrismaClient()
-  const rabbitMq = new RabbitMQAdapter()
-  await rabbitMq.connect().then(() => {
-    const email = new EmailExternal(rabbitMq)
-    const repositoryFactory = new RepositoryFactoryDatabase(client)
-    const usecase = new UsecaseFactory(repositoryFactory, email, RedisAdapter.getInstance())
-    new MainController(httpServer, usecase)
-    httpServer.listen(Number(process.env.PORT || 3000), process.env.HOST?.toString()!)
+  const email = new EmailExternal(rabbitMq)
+  const repositoryFactory = new RepositoryFactoryDatabase(client)
+  const usecase = new UsecaseFactory(repositoryFactory, email, RedisAdapter.getInstance())
+  new MainController(httpServer, usecase)
+  httpServer.listen(Number(process.env.PORT || 3000), process.env.HOST?.toString()!)
+}
+async function connectingToRabbitMQ(rabbitMq: RabbitMQAdapter) {
+  rabbitMq.connect().then(() => {
+    logger.info("[RABBITMQ]: Connected to RabbitMQ")
   }).catch((e: Error) => {
-    console.log(e.message)
+    logger.error("[RABBITMQ]: Error connected to RabbitMQ: " + e.message)
     process.exit(1)
-  })
+  });
 }
 main()
